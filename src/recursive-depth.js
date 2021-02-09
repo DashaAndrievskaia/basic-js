ed54cbfae8cdb2ea1579be7a1c4d0a8b244954df
Daria <andrievskaya.dd@gmail.com>
2021-02-09 19:14:05 +0100

    feat(recursiveDepth):  implement class method to calculate the depth of n array

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,27 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  maxDepth = 0;
+  currentDepth = 0;
+
+  calculateDepth(arr) {
+    this.currentDepth++;
+    if (arr.length > 0) {
+      for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (Array.isArray(element)) {
+          this.calculateDepth(element);
+        }
+      }
+    }
+
+    if (this.currentDepth > this.maxDepth) {
+      this.maxDepth = this.currentDepth;
+    }
+    this.currentDepth--;
+
+    if (this.currentDepth == 0) {
+      const maxDepthInCycle = this.maxDepth;
+      this.maxDepth = 0;
+      return maxDepthInCycle;
+    }
   }
 };
