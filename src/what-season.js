375b189c14c5faf907def8d9d4e9d26a089fc4e4
Daria <andrievskaya.dd@gmail.com>
2021-02-09 18:12:10 +0100

    feat(whatSeason):  implement function to get season in date

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,25 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  const seasons = [
+    "winter",
+    "winter",
+    "spring",
+    "spring",
+    "spring",
+    "summer",
+    "summer",
+    "summer",
+    "fall",
+    "fall",
+    "fall",
+    "winter"
+  ];
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (date.getMonth !== Date.prototype.getMonth) {
+    throw new Error();
+  }
+
+  return seasons[date.getMonth()];
 };
