fbbc02c4f9d86387c0886f86a104eaf2f692eff6
Daria <andrievskaya.dd@gmail.com>
2021-02-20 20:47:25 +0300

    feat(extendedRepeater): implement the function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  let result = '';
+  options.separator = options.separator || '+';
+  options.additionSeparator = options.additionSeparator || '||';
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (!options.repeatTimes) {
+    return str + options.addition;
+  }
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    let additionSeparator = options.additionSeparator;
+    let additionString = '';
+    if (i === options.repeatTimes - 1) {
+      options.separator = '';
+    }
+
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      if (j === options.additionRepeatTimes - 1) {
+        additionSeparator = '';
+      }
+      additionString += options.addition + additionSeparator;
+    }
+    result += str + additionString + options.separator;
+  }
+
+  return result;
 };
-  
