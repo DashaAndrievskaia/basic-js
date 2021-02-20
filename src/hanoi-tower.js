d8dab30b64a457e83848efe507f2c502298c9864
Daria <andrievskaya.dd@gmail.com>
2021-02-20 20:45:33 +0300

    refactor: formatting

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,13 +1,8 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    let turns =  Math.pow(2, disksNumber) - 1;
-    let seconds = Math.floor(3600 / turnsSpeed  * turns);
-    let tower = {
-      turns: turns,
-      seconds: seconds
-    };
-    tower.turns = turns;
-    tower.seconds = seconds;
-    return tower;
+  const turns = Math.pow(2, disksNumber) - 1;
+  const seconds = Math.floor(3600 / turnsSpeed * turns);
+  return {
+    turns: turns,
+    seconds: seconds
   };
+};
